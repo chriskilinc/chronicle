966bb04a922f75caa3604ac9708c2f93b22039c7
Christopher Kilinc <christopher.kilinc@if.se>
2018-12-19 15:56:12 +0100

    Added Routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const port = process.env.PORT || 3001; //  Configures the application PORT to either the current enviorments port OR if enviorment port is not set, port '3000'
+const express = require("express");
+const app = express();
+
+const indexRoute = require("./routes/indexRoute");
+const apiRoute = require("./routes/apiRoute");
+const logRoute = require("./routes/logRoute");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+//  Index Route
+app.use("/", indexRoute);
+
+//  Api Route
+app.use("/api", apiRoute);
+
+//  Log Route
+app.use("/api/log", logRoute);
+
+app.listen(port, () => console.log(`Listening on port ${port}....`));
