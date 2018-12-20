163be8281118d8ef761f1a72911ca0a84c8a8b7c
Christopher Kilinc <christopher.kilinc@if.se>
2018-12-20 15:41:48 +0100

    Log routing and exampleLog.json

--- src/routes/logRoute.js
+++ src/routes/logRoute.js
@@ -1,18 +1,28 @@
 const router = require("express").Router();
 const R = require("Ramda");
 
+let state = [];
+
 router.get("/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ log: "log" }));
+  res.send(JSON.stringify(state));
 });
 
 router.post("/", (req, res) => {
+  console.log("POST api/log");
   res.setHeader("Content-Type", "application/json");
 
   if (R.isEmpty(req.body)) {
     res.status(400).send(JSON.stringify({ error: "Bad Request (400)" }));
   } else {
-    res.send(JSON.stringify({ log: "log" }));
+    //  Save to Database
+    let log = req.body;
+    log.systemTime = new Date(Date.now())
+
+    state.push(log);
+
+    //  Debugging
+    res.send(JSON.stringify(req.body));
   }
 });
 
