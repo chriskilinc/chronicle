b2bc7015a4490f3ee20ae9369001214c9657d015
chriskilinc <chriskilinc@gmail.com>
2019-01-19 13:53:37 +0100

    Api Credential Validation

--- src/routes/logRoute.js
+++ src/routes/logRoute.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const R = require('Ramda');
 const pjson = require('../../package');
-const APIKEY = process.env.APIKEY;
+const APIKEY = process.env.APIKEY || '00000000';
 
 let state = [];
+
+isValidCredentials = req => {
+  if (req.body && req.body.apikey) {
+    if (req.body.apikey == APIKEY) {
+      //  Valid
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
 
 router.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -11,15 +22,22 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  if (!this.isValidCredentials()) {
+    res.status(500).send('Could not validate apikey on body');
+  }
   const dateFull = new Date(Date.now());
-  console.log(`POST api/log : ${dateFull.toISOString()}`);
+  console.log(`INITIAL | POST api/log : ${dateFull.toISOString()}`);
   res.setHeader('Content-Type', 'application/json');
 
+  this.validateCredentials(req, res);
+
   if (R.isEmpty(req.body)) {
+    console.log(`FAILED | POST api/log : ${dateFull.toISOString()}`);
     res.status(400).send(
       JSON.stringify({
         error: 'Bad Request (400)',
         information: {
+          apikey: '',
           text: 'Example on how a log looks like',
           example: {
             logTime: '2019-01-17T20:45:44.652Z',
@@ -39,14 +57,17 @@ router.post('/', (req, res) => {
       })
     );
   } else {
-    //  Save to Database
+    //  Create Meta Data
     let log = req.body;
     log.meta = {
       created: dateFull,
       logger: `${pjson.name} v${pjson.version}`
     };
 
-    state.push(log);
+    //  Save to Database
+    state.push(log); //  fake db
+
+    console.log(`SUCCESS | POST api/log : ${dateFull.toISOString()}`);
 
     //  Debugging
     res.send(JSON.stringify(log));
